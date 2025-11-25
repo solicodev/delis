29aec975d2c06521e32e39b4e08196f1136d85e2
m.ememighamsari <mahsaemami4@gmail.com>
2025-11-25 12:16:12 +0330

    about , footer

--- footer.php
+++ footer.php
@@ -1,30 +1,31 @@
 </main><!-- /#main -->
+<section id="top-footer">
+</section>
 <footer>
     <section id="footer">
-
         <div class=" wrapper p-md-5">
             <div class="container last-footer">
                 <div class="row justify-content-between align-items-center">
-                    <div class="col-12 col-md-3  text-start">
+                    <div class="col-12 col-md-3  text-end">
 
                         <img src="<?php echo get_template_directory_uri() ?>/assets/images/Group152.png" alt="">
-                        <p>دسر خوشمزه‌ی تو!</p>
+                        <p>دِلیس، برند خلاق گروه سولیکو است که از سال ۱۳۹۸ فعالیت خود را آغاز کرده تا با دسرهای جذابش، لحظه‌ها را شیرین‌تر کند. </p>
 
                     </div>
                     <div class="col-12 col-md-5 ">
                         <div class="row box-center">
                             <div class="col-md-6 ">
                                 <div class="link-Container position-relative d-flex flex-column col-6 col-md-12 ">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/request.svg"
-                                         class="link-Image"/>
+
+                                <span class="text-hover">درباره ما</span>
                                     <a href="<?php echo home_url('story'); ?>" class="link-Text stretched-link"> قصه‌ی
-                                        دلیس</a>
+                                         دلیس</a>
                                     <div class="border-btm">
                                     </div>
                                 </div>
                                 <div class="link-Container position-relative d-flex flex-column col-6 col-md-12">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/request.svg"
-                                         class="link-Image"/>
+                                    <span class="text-hover">پاسخ های شما</span>
+
                                     <a href="<?php echo home_url('faq'); ?>" class="link-Text stretched-link"> سوال‌های
                                         شما</a>
                                     <div class="border-btm">
@@ -33,8 +34,8 @@
                             </div>
                             <div class="col-md-6">
                                 <div class="link-Container position-relative d-flex flex-column col-6 col-md-12">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/request.svg"
-                                         class="link-Image"/>
+                                    <span class="text-hover">خوشمزه</span>
+
                                     <a href="<?php echo home_url('history'); ?>" class="link-Text stretched-link">دسرهای
                                         دلیس</a>
                                     <div class="border-btm">
@@ -42,8 +43,8 @@
                                 </div>
 
                                 <div class="link-Container position-relative d-flex flex-column col-6 col-md-12">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/request.svg"
-                                         class="link-Image"/>
+                                    <span class="text-hover">همراه شو</span>
+
                                     <a href="<?php echo home_url('history'); ?>" class="link-Text stretched-link">
                                         ارتباط با دلیس</a>
                                     <div class="border-btm">
@@ -55,33 +56,8 @@
 
                     </div>
                     <div class="col-12 col-md-3 ">
-                        <div class="icon-box d-flex justify-content-between align-items-center">
-
-                            <p>از جدیدترین اخبار مطلع شوید.</p>
-
-                            <img src="<?php echo get_template_directory_uri() ?>/assets/images/Menuu.svg" alt="">
-                        </div>
-                        <div class="container">
-                            <form class="">
-                                <div class="row">
-                                    <div class="col-md-6 mb-3 mb-md-0">
-                                        <label class="form-label">نام</label>
-                                        <input type="text" class="form-control" placeholder="">
-                                    </div>
-                                    <div class="col-md-6 ">
-                                        <label class="form-label">شماره تماس</label>
-                                        <input type="tel" class="form-control" placeholder="">
-                                    </div>
-                                </div>
-
-                                <div class="mb-3 ">
-                                    <label class="form-label">ایمیل</label>
-                                    <input type="email" class="form-control" placeholder="">
-                                </div>
-
-                                <button type="submit" class="btn btn-custom px-4 py-2">ارسال</button>
-                            </form>
-                        </div>
+                        <img src="<?php echo get_template_directory_uri()?>/assets/images/Group316.png" alt="" class="img-fluid">
+                
                     </div>
 
                 </div>
@@ -96,7 +72,7 @@
 
     </section>
 </footer>
-</body>
+
 <?php
 wp_footer();
 ?>
---
+++ template-about.php
@@ -0,0 +1,145 @@
+<?php
+/* template name: about */
+
+get_header();
+
+?>
+<section id="hero-section">
+    <div class="container">
+    <div class="row justify-content-center text-center">
+        <div class="col-md-12">
+            <div class="title-group">
+                <h2>قصه دلیس</h2>
+                <img src="<?php echo get_template_directory_uri();?>/assets/images/story-delis.png" class="img-fluid">
+            </div>
+        </div>
+    </div>
+    <div class="row align-content-center align-items-center">
+        <div class="col-md-6 position-relative">
+            <h2>
+                قاشق به قاشق
+            </h2>
+<p>
+    دلیس، یک برند خلاقه که از سال 1398 وارد دنیای تولید انواع دسرها شد و در سال 1402 با یه چهره جسور و تازه اومد تا هر لحظه رو به یه جشن شیرین تبدیل کنه.
+    دسرهای دلیس فقط یه خوراکی نیستن؛ اونا کلید یه حال خوبن، یه راه برای فرار از روزمرگی و یه بهونه برای لبخند زدن. با هر قاشق دلیس، انگار یه تکه شادی به زندگیت اضافه می‌کنی.
+</p>
+            <img src="<?php echo get_template_directory_uri()?>/assets/images/Group212.png" alt=""  class="girl-bit">
+
+        </div>
+        <div class="col-md-6">
+            <img src="<?php echo get_template_directory_uri()?>/assets/images/abut-1.png" alt="" class="img-fluid">
+        </div>
+    </div>
+    </div>
+    <div class="col-md-5">
+    </div>
+</section>
+
+ <section>
+     <div class="container ">
+         <div class="row   align-content-center align-items-center" >
+
+             <div class="col-md-6">
+
+                 <img src="<?php echo get_template_directory_uri()?>/assets/images/Group234.png " alt="" class="img-fluid">
+
+             </div>
+             <div class="col-md-6">
+                 <h2>
+                     قصه شیرین دلیس
+                 </h2>
+                 <p class="">
+                     دلیس با یک رویای بزرگ متولد شد. پر کردن لحظه‌هاتون با طعم‌های جادویی. با پشتوانه تجربه گروه سولیکو، شیر تازه و خالص، مواد اولیه درجه‌یک و یک دنیا خلاقیت، دلیس سبدی رنگارنگ از دسرهای بومی و جهانی خلق کرده.
+                     از میلک‌شیک‌های خنک که دلت رو می‌برن تا پودینگ‌های خامه‌ای، موس‌های رویایی، پاناکوتای شیک، ژله‌های پرجنب‌وجوش و ماست‌های میوه‌ای پرطراوت، ما برای هر لحظه‌ت یه طعم خاص داریم. دلیس یعنی شکستن کلیشه‌ها و خلق دسرهایی که نه فقط شکمت، بلکه قلبت رو هم سیر می‌کنه.                 </p>
+             </div>
+         </div>
+
+
+     </div>
+
+
+ </section>
+
+<section id="mission" >
+    <div class="container position-relative">
+        <div class="row   align-content-center align-items-center" >
+<div class="">
+    <img src="<?php echo get_template_directory_uri()?>/assets/images/heart-beat.svg" alt="" class="blink heart-beat ">
+</div>
+
+            <div class="col-md-6">
+                
+                <h2>
+                    ماموریت دلیس
+
+                </h2>
+                <p>
+                    ما تو دلیس می‌خوایم هر قاشق، یک قصه جدید باشه. از مهمونی‌های پرهیجان تا لحظه‌های دنج تنهایی، دلیس کنارته تا روزت رو بسازی، عزیزات رو شگفت‌زده کنی و زندگیت رو پر از انرژی و حال خوب کنی. ما فقط دسر نمی‌سازیم؛ حس زندگی رو بهت هدیه می‌دیم.
+                </p>
+            </div>
+            <div class="col-md-6">
+                <img src="<?php echo get_template_directory_uri()?>/assets/images/Group291.png " alt="" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</section>
+<section id="value-box">
+<div class="container">
+    <div class="row text-center">
+        <div class="col-md-12 position-relative">
+            <div class="title-group">
+                <h2>ارزش‌های دلیس</h2>
+                <img src="<?php echo get_template_directory_uri();?>/assets/images/Group307.png" class="img-fluid">
+            </div>
+<p>دلیس یعنی خودِ لذت، یعنی یک روش زندگی پر از حس خوب</p>
+            <div class="border-line"></div>
+        </div>
+    </div>
+
+    <div class="row text-center justify-content-center align-items-center">
+        <div class="col-md-3">
+            <div class="card text-center  border-0 ">
+                <div class="card-body  serious-box ">
+                    <div class="outline">
+                        <svg width="100%" height="100%">
+                            <rect width="100%" height="100%" fill="none" />
+                        </svg>
+                    </div>
+                    <img src="<?php echo get_template_directory_uri()?>/assets/images/Group100.png" alt="">
+                    <h2>خوشمزگی بی‌حد</h2>
+                    <p>طعمی که با یک قاشق،<br> کل روزت رو زیر و رو می‌کنه.</p>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card  border-0  ">
+                <div class="card-body  serious-box ">
+                    <img src="<?php echo get_template_directory_uri()?>/assets/images/Group100.png" alt="">
+                    <h2>تنوع بی‌نهایت</h2>
+                    <p>طعمی که با یک قاشق،<br> کل روزت رو زیر و رو می‌کنه.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-center border-0">
+                <div class="card-body  serious-box ">
+                    <img src="<?php echo get_template_directory_uri()?>/assets/images/Group100.png" alt="">
+                    <h2>شادی لحظه‌ای </h2>
+                    <p>طعمی که با یک قاشق،<br> کل روزت رو زیر و رو می‌کنه.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-center  border-0">
+                <div class="card-body serious-box ">
+                    <img src="<?php echo get_template_directory_uri()?>/assets/images/Group100.png" alt="">
+                    <h2>تازگی و جسارت</h2>
+                    <p>طعمی که با یک قاشق،<br> کل روزت رو زیر و رو می‌کنه.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+<?php get_footer(); ?>
